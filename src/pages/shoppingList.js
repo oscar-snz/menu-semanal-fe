aa0df84860af0df975ed8ffb56a28d2d32b9690f
Oscar Sanchez <oscsnz@gmail.com>
2024-03-03 20:13:43 -0600

    fixed

--- src/pages/shoppingList.js
+++ src/pages/shoppingList.js
@@ -13,14 +13,43 @@ const ShoppingListPage = () => {
   const [currentWeek, setCurrentWeek] = useState(startOfWeek(new Date(), { weekStartsOn: 1 }));
   const [shoppingList, setShoppingList] = useState(null);
   const [weeklyMenuExists, setWeeklyMenuExists] = useState(false);
+  const [inventory, setInventory] = useState([]);
   const { token } = useAuth();
   const config = { headers: { Authorization: `Bearer ${token}` } };
 
-  useEffect(() => {
-    checkWeeklyMenuExists();
-    fetchShoppingList();
-  }, [currentWeek]);
+  const levenshteinDistance = (a, b) => {
+    if (!a.length) return b.length;
+    if (!b.length) return a.length;
+  
+    const matrix = [];
+  
+    // Increment along the first column of each row
+    for (let i = 0; i <= b.length; i++) {
+      matrix[i] = [i];
+    }
+  
+    // Increment each column in the first row
+    for (let j = 0; j <= a.length; j++) {
+      matrix[0][j] = j;
+    }
+  
+    // Fill in the rest of the matrix
+    for (let i = 1; i <= b.length; i++) {
+      for (let j = 1; j <= a.length; j++) {
+        if (b.charAt(i - 1) == a.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1];
+        } else {
+          matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+                                  Math.min(matrix[i][j - 1] + 1, // insertion
+                                           matrix[i - 1][j] + 1)); // deletion
+        }
+      }
+    }
+  
+    return matrix[b.length][a.length];
+  };
 
+  
   const pluralizeWord = (word, quantity) => {
     const lastChar = word[word.length - 1].toLowerCase();
     const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -34,7 +63,53 @@ const ShoppingListPage = () => {
     }
     return word;
   };
+
   
+  // Paso 2 y 3: Comprobar artículo en inventario y generar mensaje
+  const checkItemInInventory = (item) => {
+    let message = '';
+    const threshold = 0.8; // Umbral para considerar una coincidencia
+    let hasMatch = false; // Para rastrear si encontramos alguna coincidencia
+    
+    inventory.forEach(inventoryItem => {
+      const similarity = 1 - levenshteinDistance(item.food.toLowerCase(), inventoryItem.nombreAlimento.toLowerCase()) / Math.max(item.food.length, inventoryItem.nombreAlimento.length);
+      
+      if (similarity >= threshold) {
+        hasMatch = true; // Marcamos que encontramos una coincidencia
+        const itemMeasurePluralized = pluralizeWord(item.measure, item.quantity);
+        const inventoryMeasurePluralized = pluralizeWord(inventoryItem.unidad.nombre, inventoryItem.cantidad);
+        
+        if (item.measure === inventoryItem.unidad.nombre && item.quantity <= inventoryItem.cantidad) {
+          // Caso: Tienes suficiente en inventario
+          message = `Tienes suficiente ${inventoryItem.nombreAlimento}, tienes ${inventoryItem.cantidad} ${inventoryMeasurePluralized} en tu inventario.`;
+        } else {
+          // Caso: Necesitas más pero tienes algo en inventario
+          message = `Necesitas ${item.quantity} ${itemMeasurePluralized} de ${item.food}, pero tienes ${inventoryItem.cantidad} ${inventoryMeasurePluralized} en tu inventario.`;
+        }
+      }
+    });
+    
+    return message;
+  };
+  
+  
+
+  useEffect(() => {
+    checkWeeklyMenuExists();
+    fetchShoppingList();
+    fetchInventory();
+  }, [currentWeek]);
+
+  const fetchInventory = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/api/inventario`, config);
+      setInventory(response.data[0].articulos);
+    } catch (error) {
+      setInventory([]);
+      console.error('Error fetching inventory:', error);
+    }
+  };
+
   const checkWeeklyMenuExists = async () => {
     const weekStart = format(currentWeek, 'yyyy-MM-dd');
     try {
@@ -113,10 +188,28 @@ const ShoppingListPage = () => {
       }
     }}>
       {shoppingList.items.map((item) => (
-        <ListItem key={item._id}>
-          <ListItemText primary={`${item.quantity} ${pluralizeWord(item.measure, item.quantity)} ${item.food}`} />
-        </ListItem>
-      ))}
+  <ListItem key={item._id} alignItems="flex-start">
+    <ListItemText
+      primary={
+        <React.Fragment>
+          <Typography component="span" variant="body1">
+            {`${item.quantity} ${pluralizeWord(item.measure, item.quantity)} ${item.food}`}
+          </Typography>
+        </React.Fragment>
+      }
+      secondary={
+        checkItemInInventory(item) && (
+          <Typography component="div" variant="body2" style={{ marginTop: '4px' }}>
+            {checkItemInInventory(item)}
+          </Typography>
+        )
+      }
+    />
+  </ListItem>
+))}
+      <ListItem>
+      
+      </ListItem>
     </List>
     <Button variant="contained" color="primary" onClick={handleCreateOrUpdateList} sx={{ mt: 2 }}>
       Actualizar lista de compras
